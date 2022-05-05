85951f9ef753a614e734dc642b097f102d046f5b
JulianMontu <julamontufar@misena.edu.co>
2022-05-05 13:31:13 -0500

    cambios cuenta ahorro

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { CuentaCorriente } from "./CuentaCorriente.js";
+import { CuentaAhorro } from "./CuentaAhorro.js";
 import { Cliente } from "./Cliente.js";
 
 const cliente1 = new Cliente("Sofia", "111", "001");
 const cuentaSofia = new CuentaCorriente(cliente1, 4000000, "1230", "12-32");
 console.log(cliente1);
 let saldo = cuentaSofia.valorSaldo();
-console.log(cuentaSofia.cantidadCuentas)
+console.log(cuentaSofia.cantidadCuentas);
+
+const cuentaAhorroSofia = new CuentaAhorro(cliente1, 0, "001", "920");
+console.log(cuentaSofia)
+console.log(cuentaAhorroSofia)
