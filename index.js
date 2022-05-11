2e0dc6714a6fb6fd5170948f4332b8ececf6296b
JulianMontu <julamontufar@misena.edu.co>
2022-05-10 19:25:40 -0500

    Organizando el index

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-
 import { Cliente } from "./Cliente.js";
-import { Cuenta } from "./Cuenta.js";
 import { CuentaAhorro } from "./CuentaAhorro.js";
 import { CuentaCorriente } from "./CuentaCorriente.js";
 import { CuentaNomina } from "./CuentaNomina.js";
 
-
+/**
+ * Clientes
+ */
 const cliente1 = new Cliente("Sofia", "111", "001");
-
-const cuentaSofia = new CuentaCorriente(cliente1, "1230", "12-32");
-const cuentaNominaSofia = new CuentaNomina(cliente1,'Bancolombia','13-22',0)
-//console.log(cliente1);
-let saldo = cuentaSofia.valorSaldo();
-console.log(saldo)
-//console.log(cuentaSofia.cantidadCuentas);
-
-const cuentaAhorroSofia = new CuentaAhorro(cliente1,'popular','12345',0);
-
-
-console.log(cuentaSofia);
-cuentaSofia.depositoEnCuenta(150);
-console.log(cuentaSofia.valorSaldo());
-cuentaSofia.retirarDeCuenta(70);
-console.log(cuentaSofia.valorSaldo());
+/**
+ * Cuentas Ahorros,nomina,corrientw
+ */
+const cuentaCorrienteSofia = new CuentaCorriente(cliente1, "1230", "12-32");
+const cuentaNominaSofia = new CuentaNomina(cliente1, "Bancolombia", "13-22", 0);
+const cuentaAhorroSofia = new CuentaAhorro(cliente1, "popular", "12345", 0);
+/**
+ * consultar el saldo
+ */
+let saldo = cuentaCorrienteSofia.valorSaldo();
+console.log(saldo);
+/**
+ * Cuenta Corriente  Cliente Sofia
+ * Depositos
+ * Retiros
+ */
+console.log(cuentaCorrienteSofia);
+cuentaCorrienteSofia.depositoEnCuenta(150);
+console.log(cuentaCorrienteSofia.valorSaldo());
+cuentaCorrienteSofia.retirarDeCuenta(70);
+console.log(cuentaCorrienteSofia.valorSaldo());
+/**
+ * Cuenta Ahorro cliente Sofia
+ * Depositos
+ * Retiros
+ */
 console.log(cuentaAhorroSofia);
 cuentaAhorroSofia.depositoEnCuenta(200);
 console.log(cuentaAhorroSofia.valorSaldo());
 cuentaAhorroSofia.retirarDeCuenta(120);
 console.log(cuentaAhorroSofia.valorSaldo());
-
+/**
+ * Cuenta Nomina cliente Sofia
+ * Depositos
+ * Retiros
+ */
 console.log(cuentaNominaSofia);
 cuentaNominaSofia.depositoEnCuenta(250);
 console.log(cuentaNominaSofia.valorSaldo());
